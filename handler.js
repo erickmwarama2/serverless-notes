389469278351cd25f82186849b96979068211e17
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-01-30 10:06:15 +0300

    update notes endpoint

--- handler.js
+++ handler.js
@@ -2,7 +2,16 @@
 
 module.exports.createNote = async (event) => {
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify("A new note created")
+  };
+};
+
+module.exports.updateNote = async (event) => {
+  let notesid = event.pathParameters.id;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Note with id: ${notesid} has been updated`)
   };
 };
