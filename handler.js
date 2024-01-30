b2f523b21809e959fb263b293e6f5b283ba7dc65
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-01-30 11:08:57 +0300

    create notes to dynamodb

--- handler.js
+++ handler.js
@@ -1,10 +1,33 @@
 'use strict';
 
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new DynamoDB.DocumentClient({region: "eu-west-1"});
+
 module.exports.createNote = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify("A new note created")
-  };
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: "notes",
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body
+      },
+      ConditionExpression: "attribute_not_exists(notesId)"
+    };
+
+    await documentClient.put(params).promise();
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(data)
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error.message)
+    }
+  }
 };
 
 module.exports.updateNote = async (event) => {
