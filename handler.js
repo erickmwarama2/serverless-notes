9aa93c5e4cd8a6c3b46a1edb2d0b31e9d52ddb91
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-01-31 11:38:43 +0300

    create update endpoint

--- handler.js
+++ handler.js
@@ -2,12 +2,13 @@
 
 const DynamoDB = require("aws-sdk/clients/dynamodb");
 const documentClient = new DynamoDB.DocumentClient({region: "eu-west-1"});
+const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
 
 module.exports.createNote = async (event) => {
   let data = JSON.parse(event.body);
   try {
     const params = {
-      TableName: "notes",
+      TableName: NOTES_TABLE_NAME,
       Item: {
         notesId: data.id,
         title: data.title,
@@ -32,6 +33,38 @@ module.exports.createNote = async (event) => {
 
 module.exports.updateNote = async (event) => {
   let notesid = event.pathParameters.id;
+  let data = JSON.parse(event.body);
+
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: {
+        notesId
+      },
+      UpdateExpression: `set #title = :title, #body = :body`,
+      ExpressionAttributeNames: {
+        '#title': 'title',
+        '#body': 'body'
+      },
+      ExpressionAttributeValues: {
+        ':title': data.title,
+        ':body': data.body
+      },
+      ConditionExpression: 'attribute_exists(notesId)'
+    }
+
+    await documentClient.update(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error.message)
+    }
+  }
 
   return {
     statusCode: 200,
